4555da33356a0daa8eed0d7f5e883a21fa1caabe
Ikhtiar rahman <iktiar2002@gmail.com>
2017-10-13 15:05:15 +0600

    Rest api for: 1. Event update by id 2. Event post 3. Event delete by id

--- routes/events.js
+++ routes/events.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb://localhost/events_test', {
 );
 
 //load all models  
-require('../models/events');
+const Event  = require('../models/events')
 
 /* GET events listing. */
 router.get('/', function(req, res, next) {
@@ -31,20 +31,97 @@ router.get('/', function(req, res, next) {
 
 /* GET event by eventId. */
 router.get('/:eventId', function(req, res, next) {
-   mongoose.model('events').findById(req.params.eventId, function(err,events) {
-   		res.send(events);
+   mongoose.model('events').findById(req.params.eventId, function(err,event) {
+   	    // Handle the error using the Express error middleware
+        if(err) {
+        	console.error(err.stack)
+        }
+        //if resourece not found, raise error.
+        if(!event) {
+        	res.status(404).send({message: 'No resourece found'});
+        }
+
+   	    res.status(200).send(event);
    });	
 });
 
-/* GET event by eventStartDate and endEventDate. */
+/* Post event */
+router.post('/', function(req, res, next) {
+        //set model info from request body.
+        //@todo check with proper data type, lenght, date etc.
+        if(!req.body.title || !req.body.body && !req.body.created) {
+         	res.status(400).send({message: 'Bad request'});
+        }	
+        //initiate new Event model
+        var newEvent = new Event();
+        newEvent.title = req.body.title;
+        newEvent.body  = req.body.body;
+        newEvent.created = req.body.created;
+
+        newEvent.save( function(err) {
+            // Handle the error using the Express error middleware
+	        if(err) {
+	        	console.error(err.stack)
+ 	        }
+
+	        res.send({message: 'Event created successfully.'});
+        });
+});
+
+/* Update event by eventId*/
+router.put('/:eventId', function(req, res, next) {
+   mongoose.model('events').findById(req.params.eventId, function(err,event) {
+   	    // Handle the error using the Express error middleware
+        if(err) {
+        	console.error(err.stack)
+        }
+
+        //if resourece not found, raise error.
+        if(!event) {
+        	res.status(404).send({message: 'No resourece found'});
+        }
+       
+   	    //set and update model
+        if(req.body.title) {
+        	event.title = req.body.title;
+        }
+        if(req.body.body) {
+        	event.body = req.body.body;
+        }	
+        event.updated = moment().format('YYYY-MM-DD hh:mm');
+        
+        //save event
+        event.save(function(err) {
+        	if(err) {
+        		res.send(err)
+        	}
+            res.send({message: 'Successfully updated'})
+        });
+   });	
+});
+
+/* Delete event by eventId. */
+router.delete('/:eventId', function(req, res, next) {
+   mongoose.model('events').findByIdAndRemove(req.params.eventId, function(err) {
+   	    // Handle the error using the Express error middleware
+        if(err) {
+        	console.error(err.stack)
+        }
+        
+   	    res.send({message: 'Successfully deleted'})
+   });	
+});
+
+
+/* GET event by Month number(starts from 0) */
 router.get('/month/:monthNo', function(req, res, next) {
 
 	var year = moment().format('YYYY');
 	const startOfMonth = moment([year, req.params.monthNo]).startOf('month').format('YYYY-MM-DD hh:mm');
     const endOfMonth   = moment([year, req.params.monthNo]).endOf('month').format('YYYY-MM-DD hh:mm');
-    //res.send(startOfMonth+' to '+endOfMonth);
     
     mongoose.model('events').find({"created" : {"$gte": startOfMonth,  "$lt":endOfMonth }} , function(err,events) {
+   		
    		res.send(events);
    });	
 });
